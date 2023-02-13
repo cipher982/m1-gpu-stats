b448f4a1f62655e07d8918cf879f8fb2657d6431
David Rose <david010@gmail.com>
2023-02-13 19:44:55 +0100

    get heaps

--- gpu/main.cpp
+++ gpu/main.cpp
@@ -13,20 +13,46 @@
 #include <QuartzCore/QuartzCore.hpp>
 #include <iostream>
 #include <CoreVideo/CVDisplayLink.h>
+#include <chrono>
+#include <thread>
 
 int main()
 {
-    MTL::Device *device = MTL::CreateSystemDefaultDevice();
-
+    MTL::Device *pDevice = MTL::CreateSystemDefaultDevice();
+    if (!pDevice)
+    {
+        std::cout << "Failed to create Metal device." << std::endl;
+        return 1;
+    }
     // Print the name of the device
-    std::cout << "Device: " << device->name() << std::endl;
+    std::cout << "Device: " << pDevice->name() << std::endl;
 
     // Check whether it uses unified memory
-    std::cout << "Uses unified memory: " << device->hasUnifiedMemory() << std::endl;
+    std::cout << "Uses unified memory: " << pDevice->hasUnifiedMemory() << std::endl;
 
     // Current allocated size
-    std::cout << "Current allocated size: " << device->currentAllocatedSize() << std::endl;
+    std::cout << "Current allocated size: " << pDevice->currentAllocatedSize() << std::endl;
 
-    device->release();
+    MTL::HeapDescriptor* pHeapDescriptor = MTL::HeapDescriptor::alloc()->init();
+    pHeapDescriptor->setStorageMode( MTL::StorageModeShared );
+    pHeapDescriptor->setSize( 1024 * 1024 * 1024 );
+    
+    MTL::Heap* pHeap = pDevice->newHeap( pHeapDescriptor );
+
+    MTL::HeapDescriptor* pHeapDescriptor2 = MTL::HeapDescriptor::alloc()->init();
+    pHeapDescriptor2->setStorageMode( MTL::StorageModePrivate );
+    pHeapDescriptor2->setSize( 1024 * 1024 * 1024 );
+    
+    MTL::ResourceOptions options = MTL::ResourceStorageModeShared;
+
+
+
+    while (true)
+    {
+        size_t usedBytes = pHeap->getUsedSize();
+        size_t totalBytes = pHeap->getSize();
+        std::cout << "Used memory: " << usedBytes << " bytes, Available memory: " << totalBytes - usedBytes << " bytes." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
     return 0;
 }
